6615178557ad3ab9c0d906cdc481c7c7fe314c4a
Michael <michael.danielczok@gmail.com>
2016-05-27 16:04:41 +0200

    Debug-Linnien aus Google.js gelöscht.

--- bin/controls/Google.js
+++ bin/controls/Google.js
@@ -24,13 +24,7 @@ define('package/quiqqer/socialshare/bin/controls/Google', [
         $onImport: function () {
             this.getElm().addEvent('click', function (event) {
                 event.stop();
-                var link1 = 'http://t3n.de/news/wirecutter-affiliate-publishing-710067/';
-                var link2 = 'http://www.dw.com/de/g7-droht-russland-mit-weiteren-sanktionen/a-19285537';
-                var link3 = 'http://technowinki.onet.pl/militaria/uss-zumwalt-amerykanska-marynarka-wprowadza-do-sluzby-niezwykly-okret/719erw';
-                var link4 = 'http://www.sueddeutsche.de/wissen/regierung-in-der-kritik-australien-streicht-verweise-auf-klimaschaeden-in-un-bericht-1.3008012'
-                var href = 'https://plus.google.com/share?url=' + link4;
-                window.open(href, 'social', 'height=700,width=492');
-                console.log('href');
+                window.open(this.get('href'), 'social', 'height=700,width=492');
             });
         }
     });
