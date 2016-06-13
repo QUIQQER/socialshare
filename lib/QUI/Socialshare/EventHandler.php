886ff3b0f5517d400d16a1ea3cc90f36df6f031c
Michael <michael.danielczok@gmail.com>
2016-06-13 13:46:48 +0200

    Erweiterung des Socialshares um Events - neue Klasse.

---
+++ lib/QUI/Socialshare/EventHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file contains \QUI\Socialshare\EventHandler
+ */
+
+namespace QUI\Socialshare;
+
+use QUI;
+
+/**
+ * Class Events
+ *
+ * @author www.pcsg.de (Michael Danielczok)
+ */
+class EventHandler
+{
+    /**
+     * @param \QUI\Template $Template
+     */
+    public static function onTemplateGetHeader($Template)
+    {
+
+    }
+
+}
