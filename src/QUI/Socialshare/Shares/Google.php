6c3089bddefa3ab03c9d1541172f84ddc3a08084
Michael <michael.danielczok@gmail.com>
2017-03-01 16:43:51 +0100

    refactor: code style

--- src/QUI/Socialshare/Shares/Google.php
+++ src/QUI/Socialshare/Shares/Google.php
@@ -87,7 +87,7 @@ class Google extends Socialshare
         curl_setopt($curl, CURLOPT_URL, "https://clients6.google.com/rpc");
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_POSTFIELDS,'[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . rawurldecode($countUrl) . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]');
+        curl_setopt($curl, CURLOPT_POSTFIELDS, '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . rawurldecode($countUrl) . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]');
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
         $curl_results = curl_exec($curl);
