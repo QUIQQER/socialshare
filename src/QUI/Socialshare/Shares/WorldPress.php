907d87e647de3d0691cf157e0962adecef7bb83e
Torsten <torsten@Torsten.Fink-pcsg>
2019-10-18 12:35:47 +0200

    feat: adds WorldPress (quiqqer/socialshare#26)

---
+++ src/QUI/Socialshare/Shares/WorldPress.php
@@ -0,0 +1,88 @@
+<?php
+
+
+namespace QUI\Socialshare\Shares;
+
+use QUI;
+use QUI\Socialshare\Socialshare;
+/**
+ * Facebook class for social share
+ *
+ * @author  www.pcsg.de (Torsten Fink)
+ * @package quiqqer/socialshare
+ */
+class WorldPress extends Socialshare
+{
+    public function __construct($params = array())
+    {
+        $this->setAttribute('data-qui', 'package/quiqqer/socialshare/bin/controls/WorldPress');
+        parent::__construct($params);
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \QUI\Socialshare\Socialshare::getName()
+     */
+    public function getName()
+    {
+        return 'quiqqer-socialshare-worldpress';
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \QUI\Socialshare\Socialshare::getLabel()
+     */
+    public function getLabel()
+    {
+        return QUI::getLocale()->get('quiqqer/socialshare', 'label-worldpress');
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \QUI\Socialshare\Socialshare::getLogo()
+     */
+    public function getLogo()
+    {
+        return 'fa fa-wordpress';
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \QUI\Socialshare\Socialshare::getShareUrl()
+     */
+
+    public function getShareUrl()
+    {
+        $Request = QUI::getRequest();
+        $baseurl = $Request->getScheme() . '://' . $Request->getHttpHost() . $Request->getBasePath();
+        $baseurl = $baseurl . $_SERVER['REQUEST_URI'];
+
+        return 'https://wordpress.com/wp-admin/press-this.php?u=' . $baseurl;
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \QUI\Socialshare\Socialshare::getCount()
+     */
+    public function getCount()
+    {
+
+        return null;
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \QUI\Socialshare\Socialshare::getCountUrl()
+     */
+    public function getCountUrl()
+    {
+
+        return null;
+    }
+}
