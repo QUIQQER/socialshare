dba06881c2d1996e5f4c5b61cde6232e3a88c2e8
Torsten Fink <torsten@pcsg.de>
2020-07-20 16:24:32 +0200

    feat: adds Instapaper Social share #7

--- src/QUI/Socialshare/Manager.php
+++ src/QUI/Socialshare/Manager.php
@@ -19,6 +19,7 @@ class Manager extends QUI\Controls\Control
      */
     private static $availableSocials = array(
         'Baidu',
+        'Instapaper',
         'Buffer',
         'Blogger',
         'Digg',
