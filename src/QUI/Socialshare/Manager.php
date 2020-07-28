5a8fb39474abbef436990c8d37aaf3e5eb188673
Torsten Fink <torsten@pcsg.de>
2020-07-28 16:19:27 +0200

    feat: adds MoiMir Social share #12

--- src/QUI/Socialshare/Manager.php
+++ src/QUI/Socialshare/Manager.php
@@ -19,6 +19,7 @@ class Manager extends QUI\Controls\Control
      */
     private static $availableSocials = array(
         'Baidu',
+        'MoiMir',
         'Livejornal',
         'Liveinternet',
         'Line',
