8e20d5582b82fd6deaeff32e99c1e3569ad98bd5
Torsten Fink <torsten@pcsg.de>
2020-07-29 13:53:02 +0200

    feat: adds surfingbird social share #19

--- src/QUI/Socialshare/Manager.php
+++ src/QUI/Socialshare/Manager.php
@@ -19,6 +19,7 @@ class Manager extends QUI\Controls\Control
      */
     private static $availableSocials = array(
         'Baidu',
+        'Surfingbird',
         'RenRen',
         'Pocket',
         'Readability',
